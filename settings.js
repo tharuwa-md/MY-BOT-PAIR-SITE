ba17310f5427e08a9894fcf953e811344258b388
tharuwa-md <programertharuwa@gmail.com>
2025-04-01 17:41:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,22 @@
 module.exports = {
-    PREFIX: "DASSA-MD [KILL]>>>",
+    PREFIX: "THARUWA-MD~~~~",
     CONNECTED_MESSAGE: `
-┏━━━━━━━━━━━━━━
-┃DASSA MD SESSION IS 
-┃SUCCESSFULLY
-┃CONNECTED ✅🔥
-┗━━━━━━━━━━━━━━━
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬`
+
+*✅sᴇssɪᴏɴ ɪᴅ ɢᴇɴᴇʀᴀᴛᴇᴅ✅*
+______________________________
+╔════◇
+║『 𝐘𝐎𝐔'𝐕𝐄 𝐂𝐇𝐎𝐒𝐄𝐍 𝗧𝗛𝗔𝗥𝗨𝗪𝗔 𝐌𝐃 』
+║ You've Completed the First Step
+║ to Deploy a Whatsapp Bot.
+╚══════════════╝
+╔═════◇
+║ 『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ 𝐎𝐰𝐧𝐞𝐫: _94761180276_
+║❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029Vb5gle1JUM2RWlaBHX2K_
+║ 💜💜💜
+╚══════════════╝ 
+ 𝗧𝗛𝗔𝗥𝗨𝗪𝗔-𝗠𝗗 𝗩𝗘𝗥𝗦𝗜𝗢𝗡 0.𝟬.1
+______________________________
+
+`
 };
