9cfd8fcb206a8b5dbfb717ac09c98d5de2a4e89c
tharuwa-md <programertharuwa@gmail.com>
2025-04-01 17:38:27 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    PREFIX: "DASSA-MD [KILL]>>>",
+    CONNECTED_MESSAGE: `
+┏━━━━━━━━━━━━━━
+┃DASSA MD SESSION IS 
+┃SUCCESSFULLY
+┃CONNECTED ✅🔥
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬`
+};
